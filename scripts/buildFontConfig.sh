cc5439c73169f3aee7132b55c750aa852b8fc546
Pablo Tesone <tesonep@gmail.com>
2025-03-18 12:50:20 +0100

    Adding patch for avoiding libintl dep

--- scripts/buildFontConfig.sh
+++ scripts/buildFontConfig.sh
@@ -6,8 +6,12 @@ source "$INCLUDE_DIR/common.inc"
 
 pushd $BUILD_DIR
 
-downloadAndUntar https://freedesktop.org/software/fontconfig/release/fontconfig-$FONTCONFIG_VERSION.tar.xz fontconfig-$FONTCONFIG_VERSION.tar.xz fontconfig-$FONTCONFIG_VERSION
-buildUsingMeson Fontconfig $FONTCONFIG_VERSION fontconfig-$FONTCONFIG_VERSION -Dcache-build=disabled --force-fallback-for=intl
+#downloadAndUntar https://freedesktop.org/software/fontconfig/release/fontconfig-$FONTCONFIG_VERSION.tar.xz fontconfig-$FONTCONFIG_VERSION.tar.xz fontconfig-$FONTCONFIG_VERSION
+downloadAndUntar https://ftp2.osuosl.org/pub/blfs/conglomeration/fontconfig/fontconfig-$FONTCONFIG_VERSION.tar.xz fontconfig-$FONTCONFIG_VERSION.tar.xz fontconfig-$FONTCONFIG_VERSION
+
+applyPatch fontconfig
+
+buildUsingMeson Fontconfig $FONTCONFIG_VERSION fontconfig-$FONTCONFIG_VERSION -Dcache-build=disabled -Diconv=disabled -Dnls=disabled
 
 install_name_tool -id "@executable_path/Plugins/libfontconfig.1.dylib" lib/libfontconfig.1.dylib
 
