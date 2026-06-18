9d02ecba8f811495156cd4182cc22efe1162c604
Pablo Tesone <tesonep@gmail.com>
2026-06-18 16:42:07 +0200

    Fixing patching of SDL3

--- scripts/buildSDL3.sh
+++ scripts/buildSDL3.sh
@@ -10,7 +10,7 @@ pushd $BUILD_DIR
 
 downloadAndUntar https://github.com/libsdl-org/SDL/releases/download/release-$SDL3_VERSION/SDL3-$SDL3_VERSION.tar.gz $SRC_DIR.tar.gz $SRC_DIR
 
-applyPatch SDL2 $SRC_DIR
+applyPatch SDL3 $SRC_DIR
 
 buildUsingCMake SDL3 $SDL3_VERSION $SRC_DIR -DSDL_TESTS=OFF
 
